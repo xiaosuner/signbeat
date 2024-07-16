98cf9de32ba1e6d31d6cd3e7f524bb19e41d6f3a
孙依然 <276194839@qq.com>
2024-07-16 18:40:19 +0900

    实现route页面导航

--- src/index.js
+++ src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Chapter1 from "./pages/LearningPages/chapter1";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "pages/LearningPages/chapter1",
+    element: <Chapter1 />,
+  },
+]);
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <React.StrictMode>
@@ -13,12 +29,12 @@ function startup() {
   const rootElement = document.getElementById("root");
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
+    // <React.StrictMode>
+    //   <App />
+    // </React.StrictMode>
   )
 }
 startup();
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+
---
+++ src/pages/LearningPages/chapter1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Form } from "react-router-dom";
+
+export default function Chapter1() {
+
+    return (
+        <div id="contact">
+            hello!
+        </div>
+    );
+}
